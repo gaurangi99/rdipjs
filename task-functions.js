91c145090ae43b7755a698965cdc9634a758c6bb
gaurangi99 <imgaurangi.99@gmail.com>
2020-09-05 23:52:37 +0530

    updated files

--- task-functions.js
+++ task-functions.js
@@ -3,33 +3,36 @@ function task2_name(){
 }
 
 function task2_phoneNo(){
-    var p=document.getElementById("phone").value;
+    var pno=document.getElementById("phone").value;
     var text;
-    if(isNaN(p) || p<1000000000 || p<10000000000){
-        text='Invalid input';
+    if(isNaN(pno) || pno<1000000000 || pno<10000000000){
+        text='Invalid input!';
     }
     else{
         text='Valid input!';
     }
-    document.getElementById("pno").innerHTML=text;
+    document.getElementById("phone").innerHTML=alert(text);
 }
 
 function task2_email(){
     
 }
 
-function task3_palindrome(s) {
+function task3_palindrome() {    
+    var pd=document.getElementById("palindrome").value;
     var remove = /[^A-Za-z0-9]/g;
-    s = s.toLowerCase().replace(remove, '');
-    var size = s.length;
+    pd = pd.toLowerCase().replace(remove, '');
+    var size = pd.length;
     for (var i = 0; i < size/2; i++) {
-      if (s[i] !== s[size - 1 - i])
+      if (pd[i] !== pd[size - 1 - i])
           return false;
     }
     return true;
 }
 
-function task3_anagram(str,word){
+function task3_anagram(){
+    var str=document.getElementById("anagram_str").value;
+    var word=document.getElementById("anagram_word").value;
     var s1 = this.str.toLowerCase().split('').sort().join('').trim();
     var s2 = this.word.toLowerCase().split('').sort().join('').trim();
 
